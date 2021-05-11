4f3c46aa9a88529715c09a25e9be6c5c75bcfe8e
Shaunbus <sjbucyk@live.ca>
2021-05-11 12:27:08 -0700

    Boilerplate Code instantiated

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+//app.use(bodyParser.json());
+app.use(express.json()); //Used to parse JSON bodies
+
+// parse requests of content-type - application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Shaunbus application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
